1cdd5e334a3467af410fd29348d5d137982dfdf8
Kirby Hutton <kirby.hutton@gmail.com>
2019-02-01 05:05:59 +0000

    commit for javascript hw

---
+++ melon_counting.js
@@ -0,0 +1,35 @@
+"use strict";
+//Our customers are going to buy lots of melons!
+
+let melonsToAdd = ['Ogen', 'Horned Melon', 'Watermelon', 'Casaba',
+                 'Sharlyn', 'Xigua', 'Ogen', 'Christmas', 'Christmas',
+                 'Christmas', 'Christmas', 'Watermelon', 'Sharlyn', 'Xigua',
+                 'Cantaloupe', 'Christmas', 'Watermelon', 'Christmas',
+                 'Sharlyn', 'Christmas', 'Cantaloupe', 'Casaba', 'Cantaloupe',
+                 'Santa Claus', 'Horned Melon', 'Watermelon', 'Ogen',
+                 'Horned Melon', 'Cantaloupe', 'Xigua', 'Horned Melon', 'Sharlyn',
+                 'Horned Melon', 'Sharlyn', 'Cantaloupe', 'Christmas',
+                 'Horned Melon', 'Horned Melon', 'Horned Melon', 'Xigua', 'Xigua',
+                 'Watermelon', 'Cantaloupe', 'Casaba', 'Cantaloupe', 'Casaba',
+                 'Watermelon', 'Santa Claus', 'Casaba']
+
+
+function countMelons(melonList) {
+    //Takes in a list, and returns a dictionary with melon counts.
+
+    let melonCounts = new Map();
+
+    for (let melon of melonList) {
+        if (melonCounts.hasOwnProperty(melon)) {
+            melonCounts[melon] += 1;
+        }
+
+        else {
+                melonCounts[melon] = 1;
+
+        }
+
+}
+    return melonCounts;
+
+}
